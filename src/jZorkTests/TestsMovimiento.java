1972c714423318237d85b5fa12068c7ab59e5097
Pablo Bermudez <pbermudez@artech-consulting.com>
2020-05-18 01:39:20 -0300

    Commit obligado

--- src/jZorkTests/TestsMovimiento.java
+++ src/jZorkTests/TestsMovimiento.java
@@ -25,19 +25,104 @@ class TestsMovimiento {
 	}
 	
 	@Test
-	void testNorte() {
-		
-		Posicion actual = mapa.getPosicionActual();
-		mapa.moverNorte();
-		Posicion norte = mapa.getPosicionActual();
-		
-		actual.setY(actual.getY() + 1);
-		
-		assertEquals(actual, norte);
-		
+	void testPosicionInicial() {
+		assertEquals(new Posicion(5,5,1), mapa.getPosicionActual());
 	}
 	
+	@Test
+	void testNorte() {
+		mapa.moverNorte();
+		assertEquals(new Posicion(5,6,1), mapa.getPosicionActual());	
+	}
 	
+	@Test
+	void testNoHayNorte() {
+		mapa.moverNorte();
+		mapa.moverNorte();
+		mapa.moverNorte();
+		assertEquals(new Posicion(5,7,1), mapa.getPosicionActual());
+	}
+	
+	@Test
+	void testSur() {
+		mapa.moverSur();
+		assertEquals(new Posicion(5,4,1), mapa.getPosicionActual());
+	}
+	
+	@Test
+	void testNoHaySur() {
+		mapa.moverSur();
+		mapa.moverSur();
+		assertEquals(new Posicion(5,4,1), mapa.getPosicionActual());
+	}
+	
+	@Test
+	void testOeste() {
+		mapa.moverOeste();
+		assertEquals(new Posicion(4,5,1),  mapa.getPosicionActual());
+	}
+	
+	@Test
+	void testNoHayOeste() {
+		mapa.moverOeste();
+		mapa.moverOeste();
+		assertEquals(new Posicion(4,5,1),  mapa.getPosicionActual());
+	}
+	
+	@Test
+	void testEste() {
+		mapa.moverEste();
+		assertEquals(new Posicion(6,5,1), mapa.getPosicionActual());
+	}
+	
+	@Test
+	void testNoHayEste() {
+		mapa.moverEste();
+		mapa.moverEste();
+		assertEquals(new Posicion(6,5,1), mapa.getPosicionActual());
+	}
+	
+	@Test
+	void testIrArriba() {
+		mapa.moverNorte();
+		mapa.moverNorte();
+		mapa.moverArriba();
+		assertEquals(new Posicion(5,7,2), mapa.getPosicionActual());
+	}
+	
+	@Test
+	void testIrAbajo() {
+		mapa.moverNorte();
+		mapa.moverNorte();
+		mapa.moverAbajo();
+		assertEquals(new Posicion(5,7,0), mapa.getPosicionActual());
+	}
+	
+	@Test
+	void testNoSePuedeSubir() {
+		mapa.moverNorte();
+		mapa.moverNorte();
+		mapa.moverArriba();
+		assertEquals(new Posicion(5,7,2), mapa.getPosicionActual());
+		mapa.moverArriba();
+		assertEquals(new Posicion(5,7,2), mapa.getPosicionActual());
+	}
+	
+	@Test
+	void testNoSePuedeBajar() {
+		mapa.moverNorte();
+		mapa.moverNorte();
+		mapa.moverAbajo();
+		assertEquals(new Posicion(5,7,0), mapa.getPosicionActual());
+		mapa.moverAbajo();
+		assertEquals(new Posicion(5,7,0), mapa.getPosicionActual());
+	}
+	
+//	@Test
+//	void testIrHacia() {
+//		mapa.irHacia("Cabania");
+//		assertEquals(new Posicion(5,6,1), mapa.getPosicionActual());
+//	}
 	
 
 }
--- src/zorkPackage/Posicion.java
+++ src/zorkPackage/Posicion.java
@@ -33,6 +33,36 @@ public class Posicion {
 	public String toString() {
 		return "Posicion [x=" + x + ", y=" + y + ", z=" + z + "]";
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		result = prime * result + z;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Posicion other = (Posicion) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		if (z != other.z)
+			return false;
+		return true;
+	}
+	
+	
+	
+	
 	
 	
 	
