9c66590aca50ea8740d99f72127c66bf8e3996e2
Noeli Sasha <noese63@gmail.com>
2020-05-19 23:20:22 -0300

    Agrego test para personaje

--- src/jZorkTests/TestPersonaje.java
+++ src/jZorkTests/TestPersonaje.java
@@ -10,6 +10,7 @@ import zorkPackage.*;
 class TestPersonaje {
 
 	Personaje p;
+	Mapa mapa;
 
 	@BeforeEach
 	void setUp() throws Exception {
@@ -27,6 +28,12 @@ class TestPersonaje {
 	}
 	
 	@Test
+	void testCambiarNombre() {
+		p.setNombre("Pedro");
+		assertEquals("Pedro", p.getNombre());
+	}
+	
+	@Test
 	void testInventarioVacio() {
 		assertEquals(0, p.getInventario().size());
 	}
@@ -36,6 +43,18 @@ class TestPersonaje {
 		p.addObjeto(new Item());
 		p.addObjeto(new Item());
 		assertEquals(2, p.getInventario().size());
+	}
+	
+	@Test
+	void testArmaEquipada() {
+		assertNull(p.getArmaEquipada());
+		mapa = new Mapa();
+		mapa.setPersonajeActual(p);
+		JsonReader.construirAventura(mapa, "testMovimiento.txt");
+		Item i = mapa.getLugarActual().getItem("gato");
+		p.addObjeto(i);
+		p.equiparArma("gato");
+		assertEquals(i, p.getArmaEquipada());
 	}
 
 }
